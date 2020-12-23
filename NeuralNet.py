c6e63176be2fd4a273b9699f0bbc2d72f5ad2d96
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-12-22 22:12:46 -0500

    Implemented basic neural network to visualize

---
+++ NeuralNet.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+class NeuralNet:
+    def __init__(self):
+        self.x = 5
+
--- gradient_descent/gradient_descent.py
+++ gradient_descent/gradient_descent.py
@@ -21,4 +21,4 @@ def gradient_descent(x, y):
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([5, 7, 9, 11, 13])
 
-gradient_descent(x, y)
+#gradient_descent(x, y)
